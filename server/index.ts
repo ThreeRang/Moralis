201c28dfba88d2b2fb9f11fe4bf1889bc524aaaf
koreandrum97 <park04181@gmail.com>
2022-05-22 02:04:17 +0900

    [fix]

--- server/index.ts
+++ server/index.ts
@@ -41,24 +41,24 @@ const web3API = async () => {
   });
   console.log(price);
 
-  /*토큰 전달*/
-  // Enable web3
-  await Moralis.enableWeb3({
-    //BSC mainnet
-    chainId: 0x38,
-    privateKey: "YOUR-PRIVATE KEY",
-  });
+  // /*토큰 전달*/
+  // // Enable web3
+  // await Moralis.enableWeb3({
+  //   //BSC mainnet
+  //   chainId: 0x38,
+  //   privateKey: "YOUR-PRIVATE KEY",
+  // });
 
-  // sending 0.5 DAI tokens with 18 decimals on BSC mainnet
-  const options: Moralis.TransferOptions = {
-    type: "erc20",
-    amount: Moralis.Units.Token("0.5", 18),
-    receiver: "0x93905fd3f9b8732015f2b3Ca6c16Cbcb60ECf895",
-    contractAddress: "0x1AF3F329e8BE154074D8769D1FFa4eE058B1DBc3",
-  };
-  await Moralis.transfer(options).then((result) => {
-    console.log(result);
-  });
+  // // sending 0.5 DAI tokens with 18 decimals on BSC mainnet
+  // const options: Moralis.TransferOptions = {
+  //   type: "erc20",
+  //   amount: Moralis.Units.Token("0.5", 18),
+  //   receiver: "0x93905fd3f9b8732015f2b3Ca6c16Cbcb60ECf895",
+  //   contractAddress: "0x1AF3F329e8BE154074D8769D1FFa4eE058B1DBc3",
+  // };
+  // await Moralis.transfer(options).then((result) => {
+  //   console.log(result);
+  // });
 };
 
 web3API();
